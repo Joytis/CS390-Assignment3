4aefb5ba7267507b26d0f7428854a9202bb07374
Clark Chambers <clarkcambers94@gmail.com>
2019-04-02 18:58:04 -0500

    Works lol

--- Chambers_cs390program3.sh
+++ Chambers_cs390program3.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Example Usage
-# find . -type f -name "*.cpp" -o -name "*.h" | ./Chambers_cs390program3.sh
+# find . -type f -name "*.cpp" | ./Chambers_cs390program3.sh
 
 while read line 
 do
-    sed -i 's/[\W|\s]*unsigned char[\W|\s]*/CS390_UCHAR/g' $line      # unsigned char ->   CS390_UCHAR
-    sed -i 's/[\W|\s]*char[\W|\s]*/CS390_CHAR/g' $line                # char ->            CS390_CHAR
-    sed -i 's/[\W|\s]*unsigned short[\W|\s]*/CS390_UINT16/g' $line    # unsigned short ->  CS390_UINT16
-    sed -i 's/[\W|\s]*short[\W|\s]*/CS390_INT16/g' $line              # short ->           CS390_INT16
-    sed -i 's/[\W|\s]*unsigned int[\W|\s]*/CS390_UINT32/g' $line      # unsigned int ->    CS390_UINT32
-    sed -i 's/[\W|\s]*int[\W|\s]*/CS390_INT32/g' $line                # int ->             CS390_INT32
-    sed -i 's/[\W|\s]*float[\W|\s]*/CS390_FLOAT32/g' $line            # float ->           CS390_FLOAT32
-    sed -i 's/[\W|\s]*double[\W|\s]*/CS390_FLOAT64/g' $line           # double ->          CS390_FLOAT32
-    sed -i 's/[\W|\s]*bool[\W|\s]*/CS390_BOOL/g' $line                # bool ->            CS390_BOOL
+    sed -i 's/\bunsigned char\b/CS390_UCHAR/g' $line      # unsigned char ->   CS390_UCHAR
+    sed -i 's/\bchar\b/CS390_CHAR/g' $line                # char ->            CS390_CHAR
+    sed -i 's/\bunsigned short\b/CS390_UINT16/g' $line    # unsigned short ->  CS390_UINT16
+    sed -i 's/\bshort\b/CS390_INT16/g' $line              # short ->           CS390_INT16
+    sed -i 's/\bunsigned int\b/CS390_UINT32/g' $line      # unsigned int ->    CS390_UINT32
+    sed -i 's/\bint\b/CS390_INT32/g' $line                # int ->             CS390_INT32
+    sed -i 's/\bfloat\b/CS390_FLOAT32/g' $line            # float ->           CS390_FLOAT32
+    sed -i 's/\bdouble\b/CS390_FLOAT64/g' $line           # double ->          CS390_FLOAT32
+    sed -i 's/\bbool\b/CS390_BOOL/g' $line                # bool ->            CS390_BOOL
 done
